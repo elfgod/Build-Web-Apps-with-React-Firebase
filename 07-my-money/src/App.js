dea49468b956469d1d85b080c9e40b9e68caf239
ElfGod <albertogx@icloud.com>
2022-03-30 01:44:39 -0500

    commit 14

--- 07-my-money/src/App.js
+++ 07-my-money/src/App.js
@@ -1,7 +1,32 @@
-import './App.css'
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from 'react-router-dom/cjs/react-router-dom.min'
+import Navbar from './components/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
-function App() {
-  return <div className='App'></div>
+const App = () => {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ 07-my-money/src/pages/signup/Signup.js
@@ -0,0 +1,8 @@
+// styles
+import styles from './Signup.module.css'
+
+const Signup = () => {
+  return <div>Signup</div>
+}
+
+export default Signup
