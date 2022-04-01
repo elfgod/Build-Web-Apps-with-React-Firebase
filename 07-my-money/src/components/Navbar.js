9e33a4ce36b86b65e4d07c78b6392e8600532942
ElfGod <albertogx@icloud.com>
2022-03-31 23:36:19 -0500

    commit 17

--- 07-my-money/src/components/Navbar.js
+++ 07-my-money/src/components/Navbar.js
@@ -1,24 +1,36 @@
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-        <li>
-          <button className='btn' onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className='btn' onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )
--- 07-my-money/src/pages/login/Login.js
+++ 07-my-money/src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useLogin } from '../../hooks/useLogin'
 
 // styles
 import styles from './Login.module.css'
@@ -6,10 +7,12 @@ import styles from './Login.module.css'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { login, error, isPending } = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(email, password)
+    login(email, password)
   }
 
   return (
@@ -30,7 +33,9 @@ const Login = () => {
           value={password}
         />
       </label>
-      <button className='btn'>Login</button>
+      {!isPending && <button className='btn'>Login</button>}
+      {isPending && <button className='btn disabled'>loading</button>}
+      {error && <p>error</p>}
     </form>
   )
 }
