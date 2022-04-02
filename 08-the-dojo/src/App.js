724d89cc0cb32a9f928071552d7a31d879e735c7
ElfGod <albertogx@icloud.com>
2022-04-02 17:52:38 -0500

    Re-using Hooks, Auth, Context / Router and Pages setup / Navbar Component

--- 08-the-dojo/src/App.js
+++ 08-the-dojo/src/App.js
@@ -1,7 +1,43 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// Styles
 import './App.css'
 
+// Pages and components
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Project from './pages/project/Project'
+import Navbar from './components/Navbar'
+
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/project/:id'>
+              <Project />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
