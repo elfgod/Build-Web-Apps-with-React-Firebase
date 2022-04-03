177dcd8899714b98f7e9ceaea6a2b98b565285d8
ElfGod <albertogx@icloud.com>
2022-04-03 01:46:26 -0500

    Showing Users Online

--- 08-the-dojo/src/components/OnlineUsers.js
+++ 08-the-dojo/src/components/OnlineUsers.js
@@ -7,14 +7,17 @@ import React from 'react'
 import Avatar from './Avatar'
 
 const OnlineUsers = () => {
-  const { error, documents } = useCollection('users')
+  const { isPending, error, documents } = useCollection('users')
+
   return (
     <div className='user-list'>
       <h2>All Users</h2>
-      {error && <div className='error'>{error}</div>}
+      {isPending && <div>Loading users...</div>}
+      {error && <div>{error}</div>}
       {documents &&
         documents.map((user) => (
           <div key={user.id} className='user-list-item'>
+            {user.online && <span className='online-user'></span>}
             <span>{user.displayName}</span>
             <Avatar src={user.photoURL} />
           </div>
