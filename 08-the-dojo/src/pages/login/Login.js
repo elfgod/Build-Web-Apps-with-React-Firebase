a16e524ad5e2174500d7237ded68e5f8abbd7617
ElfGod <albertogx@icloud.com>
2022-04-02 22:11:07 -0500

    Making a Login Page

--- 08-the-dojo/src/pages/login/Login.js
+++ 08-the-dojo/src/pages/login/Login.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// Styles
+// Remember styles are global, not like css modules
+import './Login.css'
 
 const Login = () => {
-  return <div>Login</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
+  return (
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button>login</button>
+      {/* {!isPending && <button className='btn'>Login</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className='error'>{error}</div>} */}
+    </form>
+  )
 }
 
 export default Login
