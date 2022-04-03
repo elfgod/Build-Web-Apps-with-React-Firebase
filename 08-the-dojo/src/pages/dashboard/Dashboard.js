2cf21e98aa56fc31b3bacd8a2effea24039cdb55
ElfGod <albertogx@icloud.com>
2022-04-03 17:58:53 -0500

    Making a Project List / Grid

--- 08-the-dojo/src/pages/dashboard/Dashboard.js
+++ 08-the-dojo/src/pages/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import ProjectList from '../../components/ProjectList'
 import { useCollection } from '../../hooks/useCollection'
 
-// Styles
+// components
+import ProjectList from '../../components/ProjectList'
+
+// styles
 import './Dashboard.css'
 
 const Dashboard = () => {
   const { documents, error } = useCollection('projects')
+
   return (
     <div>
       <h2 className='page-title'>Dashboard</h2>
