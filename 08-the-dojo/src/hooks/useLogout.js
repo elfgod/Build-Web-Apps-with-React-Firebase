91f492b0dd0844c9789f7b8487bf05ce545cd994
ElfGod <albertogx@icloud.com>
2022-04-02 22:01:47 -0500

    Logging Users Out

--- 08-the-dojo/src/hooks/useLogout.js
+++ 08-the-dojo/src/hooks/useLogout.js
@@ -1,21 +1,27 @@
 import { useEffect, useState } from 'react'
-import { projectAuth } from '../firebase/config'
+import { projectAuth, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
-  
+  const { dispatch, user } = useAuthContext()
+
   const logout = async () => {
     setError(null)
     setIsPending(true)
 
     try {
-      // sign the user out
+      // Update online status in database
+      const { uid } = user
+      await projectFirestore
+        .collection('users')
+        .doc(uid)
+        .update({ online: false })
+
       await projectAuth.signOut()
-      
+
       // dispatch logout action
       dispatch({ type: 'LOGOUT' })
 
@@ -23,9 +29,8 @@ export const useLogout = () => {
       if (!isCancelled) {
         setIsPending(false)
         setError(null)
-      } 
-    } 
-    catch(err) {
+      }
+    } catch (err) {
       if (!isCancelled) {
         setError(err.message)
         setIsPending(false)
