0af81e6b65236430d656f6eae4a028d471f3fe5b
ElfGod <albertogx@icloud.com>
2022-04-04 00:23:43 -0500

    Completing / Deleting Projects

--- 08-the-dojo/src/pages/project/ProjectSummary.js
+++ 08-the-dojo/src/pages/project/ProjectSummary.js
@@ -1,10 +1,22 @@
 import Avatar from '../../components/Avatar'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useHistory } from 'react-router-dom'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 const ProjectSummary = ({ project }) => {
+  const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
+  const history = useHistory()
+
+  const handleClick = () => {
+    deleteDocument(project.id)
+    history.push('/')
+  }
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'>{project.name}</h2>
+        <p>{project.createdBy.displayName}</p>
         <p className='due-date'>
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
@@ -18,6 +30,11 @@ const ProjectSummary = ({ project }) => {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className='btn' onClick={handleClick}>
+          Mark as Complete
+        </button>
+      )}
     </div>
   )
 }
