4a5d0c5f5fa9137f4b3a0505199c0ea82372a136
ElfGod <albertogx@icloud.com>
2022-04-05 00:38:22 -0500

    Deleting Data

--- 09-reading-list/src/components/BookList.js
+++ 09-reading-list/src/components/BookList.js
@@ -1,16 +1,29 @@
-export default function BookList({ books }) {
+import { db } from '../firebase/config'
+// Instead of collection to get a reference to an specific collection, we can use
+// doc to get a reference to a specific documents, we use this to update and delete
+import { doc, deleteDoc } from 'firebase/firestore'
 
+const BookList = ({ books }) => {
   const handleClick = async (id) => {
-    console.log(id)
+    // console.log(id)
+    const ref = doc(db, 'books', id)
+    await deleteDoc(ref)
+
+    // Firebase documentation
+    // await deleteDoc(doc(db, 'books', id)
   }
 
   return (
-    <div className="book-list">
+    <div className='book-list'>
       <ul>
-        {books.map(book => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
+        {books.map((book) => (
+          <li key={book.id} onClick={() => handleClick(book.id)}>
+            {book.title}
+          </li>
         ))}
       </ul>
     </div>
   )
 }
+
+export default BookList
