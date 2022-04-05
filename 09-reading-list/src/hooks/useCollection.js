9f5bebfb423fd4619e09dafe8b7c49c79798abd4
ElfGod <albertogx@icloud.com>
2022-04-05 02:11:25 -0500

    Firestore Queries

--- 09-reading-list/src/hooks/useCollection.js
+++ 09-reading-list/src/hooks/useCollection.js
@@ -1,15 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { db } from '../firebase/config'
 
 // firebase imports
-import { collection, onSnapshot } from 'firebase/firestore'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
 
-export const useCollection = (c) => {
+export const useCollection = (c, _q) => {
   const [documents, setDocuments] = useState(null)
+
+  // set up query
+  const q = useRef(_q).current
 
   useEffect(() => {
     let ref = collection(db, c)
 
+    // if query is defined, add it to the ref
+    if (q) {
+      ref = query(ref, where(...q))
+    }
     const unsub = onSnapshot(ref, (snapshot) => {
       let results = []
       snapshot.docs.forEach((doc) => {
@@ -19,7 +26,7 @@ export const useCollection = (c) => {
     })
 
     return () => unsub()
-  }, [c])
+  }, [c, q])
 
   return { documents }
 }
