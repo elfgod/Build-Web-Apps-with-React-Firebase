181505e7bbd15a4610e314df774f8b46483c0ff5
ElfGod <albertogx@icloud.com>
2022-04-05 01:50:15 -0500

    Dispatching Actions

--- 09-reading-list/src/hooks/useLogin.js
+++ 09-reading-list/src/hooks/useLogin.js
@@ -1,17 +1,20 @@
 import { useState } from 'react'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // firebase imports
 import { auth } from '../firebase/config'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 
 export const useLogin = () => {
+  const { dispatch } = useAuthContext()
   const [error, setError] = useState(null)
 
   const login = (email, password) => {
     setError(null)
     signInWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        console.log('user logged in:', res.user)
+        // console.log('user logged in:', res.user)
+        dispatch({ type: 'LOGIN', payload: res.user })
       })
       .catch((err) => {
         setError(err.message)
--- 09-reading-list/src/hooks/useSignup.js
+++ 09-reading-list/src/hooks/useSignup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // firebase imports
 import { auth } from '../firebase/config'
@@ -6,12 +7,14 @@ import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 export const useSignup = () => {
   const [error, setError] = useState(null)
+  const { dispatch } = useAuthContext()
 
   const signup = (email, password) => {
     setError(null) // reset the user to error every time we try to signup a new user
     createUserWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        console.log('user signed up: ', res.user)
+        // console.log('user signed up: ', res.user)
+        dispatch({ type: 'LOGIN', payload: res.user })
       })
       .catch((err) => {
         setError(err.message)
